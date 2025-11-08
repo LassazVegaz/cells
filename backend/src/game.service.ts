62b58e1cb1b564a3205d0096171b17a3851e8c6c
LassazVegaz <lasindunuwanga@gmail.com>
2025-11-08 21:32:47 +0800

    rewards based on distance

--- backend/src/game.service.ts
+++ backend/src/game.service.ts
@@ -9,10 +9,13 @@ export enum Action {
 
 @Injectable()
 export class GameService {
-  private readonly rewards: number[] = [];
+  private readonly positionRewards: number[];
+  private readonly targetReward: number = 18 * 13;
+  private readonly distanceRewards: number[];
   private readonly gridSize = 10;
   private readonly boxesCount = this.gridSize * this.gridSize;
-  private readonly hittingWallPenalty = -2;
+  private readonly hittingWallPenalty: number;
+  private readonly targetState = this.boxesCount;
 
   private readonly actions: Record<Action, (s: number) => number> = {
     [Action.Left]: (s) => (s % this.gridSize === 1 ? s : s - 1),
@@ -23,24 +26,44 @@ export class GameService {
   };
 
   constructor() {
-    this.rewards = this.generateRewards();
+    this.distanceRewards = this.generateDistanceRewards();
+    this.hittingWallPenalty = Math.min(...this.distanceRewards) - 1;
   }
 
   getRewards(oldS: number, s: number): number {
-    if (oldS === s) return this.hittingWallPenalty; // penalty for hitting wall
-    return this.rewards[s - 1];
+    if (oldS === s) return this.hittingWallPenalty;
+    if (s === this.targetState) return this.targetReward;
+    return this.distanceRewards[s];
   }
 
   performAction(a: Action, s: number) {
     return this.actions[a](s);
   }
 
-  private generateRewards() {
+  private generatePositionRewards() {
     const _r: number[] = [];
     for (let i = 1; i < this.boxesCount; i++) {
       _r[i] = -1;
     }
     _r[this.boxesCount] = 100; // goal state
+    return _r;
+  }
+
+  private generateDistanceRewards() {
+    const _r: number[] = [];
+    const targetX = this.targetState % this.gridSize || this.gridSize;
+    let targetY = Math.floor(this.targetState / this.gridSize);
+    if (targetX !== this.gridSize) targetY += 1;
+
+    for (let i = 1; i <= this.boxesCount; i++) {
+      const x = i % this.gridSize || this.gridSize;
+      let y = Math.floor(i / this.gridSize);
+      if (x !== this.gridSize) y += 1;
+
+      const distance = Math.hypot(targetX - x, targetY - y);
+      _r[i] = -distance;
+    }
+
     return _r;
   }
 }
